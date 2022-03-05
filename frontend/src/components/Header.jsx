7d985341ac90324d8cd1db862b0b1bd01926e7f9
Tyler Robinson <tyler.robinson.business@gmail.com>
2022-03-05 17:45:57 -0500

    got logout to properly work

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
-import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { logout, reset } from "../features/auth/authSlice";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
 
 function Header() {
   const navigate = useNavigate();
@@ -9,32 +9,32 @@ function Header() {
   const { user } = useSelector((state) => state.auth);
 
   const onLogout = () => {
-    dispatch(logout());
-    dispatch(reset());
-    navigate("/");
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
   };
 
   return (
-    <header className="header">
-      <div className="logo">
-        <Link to="/">GoalSetter</Link>
+    <header className='header'>
+      <div className='logo'>
+        <Link to='/'>GoalSetter</Link>
       </div>
       <ul>
         {user ? (
           <li>
-            <button className="btn" onClick={onLogout}>
+            <button className='btn' onClick={onLogout}>
               <FaSignOutAlt /> Logout
             </button>
           </li>
         ) : (
           <>
             <li>
-              <Link to="/login">
+              <Link to='/login'>
                 <FaSignInAlt /> Login
               </Link>
             </li>
             <li>
-              <Link to="/register">
+              <Link to='/register'>
                 <FaUser /> Register
               </Link>
             </li>
